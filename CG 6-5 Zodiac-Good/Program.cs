14c8f696503161dc3f2dc1470798cf2bcfceb93d
Kristen Shea <kstar61@live.com>
2018-08-16 01:11:36 -0500

    Add project files.

---
+++ CG 6-5 Zodiac-Good/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CG_6_5_Zodiac_Good
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
